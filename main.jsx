2c4782cdf061638f3380ed4272e476210cb5095c
Pavan bhagi <bhagi8437@gmail.com>
2026-02-02 23:40:13 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Jets from './jets.jsx'
+import Tejas from './compontent/tejas.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <Jets /> */}
+    {/* <Tejas /> */}
+    <App />
+
+  </StrictMode>,
+)
